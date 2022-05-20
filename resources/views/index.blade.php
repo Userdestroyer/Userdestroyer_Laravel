f5f276cbcdec1a7d7c077054c95a205212421c02
Max Savelev <G-man74@Yandex.ru>
2022-05-20 23:18:32 +0500

    changes for the night

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Userdestroyer</title>
+    <style>
+        header {
+        width: 100%;
+        height: auto;
+        font-size: 11vw;
+        color: white;
+        background-color: black;
+        font-family: 'League Gothic', sans-serif;
+        margin: 0px;
+        padding: 0px;
+        }
+        body {
+            background-color: black;
+            margin: 0px;
+            padding: 0px;
+        }
+    </style>
 </head>
+
 <body class="antialiased">
-    <div class="header">
-    FOOKING TEST AGAIN
+    <div>
+        <header>USERDESTROYER</header>
     </div>
 </body>
 </html>
